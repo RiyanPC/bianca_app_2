f432e471e80001eb234544d721517d699435f4af
RiyanPC <locochon56303@gmail.com>
2025-08-14 09:56:57 -0500

    prueba

--- App/main/MainApp.java
+++ App/main/MainApp.java
@@ -6,6 +6,7 @@ import javax.swing.SwingUtilities;
 
 public class MainApp {
     public static void main(String[] args) {
+        //inicializar xd
         DatabaseConfig.init();
         SwingUtilities.invokeLater(() -> new EmpresasUI().setVisible(true));
     }
